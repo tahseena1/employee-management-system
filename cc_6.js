a6632146aaa488eb0fe48cee175c5043a928828b
Tahseena Badat <tahseena@usf.edu>
2025-06-28 14:53:19 +0200

    Added company class with add and list functions

--- cc_6.js
+++ cc_6.js
@@ -22,3 +22,19 @@ const emp1 = new Employee("Jim Halpert", "Sales");
 const emp2 = new Employee("Creed Bratton", "Quality Assurance");
 const emp3 = new Employee("Angela Martin", "Accounting");
 const mngr1 = new Manager("Micheal Scott", "Scranton, PA", 15);
+
+class Company {
+    constructor (employees) {
+        this.employees = [];
+    }
+    
+    addEmployee(employee){
+        this.employees.push(employee);
+    }
+
+    listEmployees(employee){
+        this.employees.forEach (emp => {
+            console.log(employee.describe());
+        });
+    }
+}
